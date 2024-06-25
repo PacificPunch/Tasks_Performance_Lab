5fde1f33867f592720aaf3f958eac4f430aa4f91
PacificPunch <ggfawn@gmail.com>
2024-06-25 17:56:37 +0300

    test
    test

---
+++ task1/task1.py
@@ -0,0 +1,27 @@
+import sys
+
+def find_path(n, m):
+    # Создаем круговой массив
+    circular_array = [i+1 for i in range(n)]
+    
+    # Находим путь
+    path = [circular_array[0]]
+    current_index = 0
+    while True:
+        current_index = (current_index + m - 1) % n
+        path.append(circular_array[current_index])
+        if circular_array[current_index] == circular_array[0]:
+            break
+    
+    return path[:-1]
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Использование: python task1 <n> <m>")
+        sys.exit(1)
+    
+    n = int(sys.argv[1])
+    m = int(sys.argv[2])
+    
+    path = find_path(n, m)
+    print("".join(map(str, path)))
